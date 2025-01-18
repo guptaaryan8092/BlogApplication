cdf3aba5588dace3e64f7bc94b8b4b685ab84f4a
guptaaryan8092 <guptaaryan8092@gmail.com>
2025-01-19 02:06:56 +0530

    password encryption

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,8 +1,14 @@
+import bcrypt from 'bcrypt';
+
 import User from '../model/user.js'
 
 export const signupUser = async (request, response) => {
     try{
-        const user = request.body;
+        // const salt = await bcrypt.genSalt();
+        // const hashedPassword = await bcrypt.hash(request.body.password, salt);
+        const hashedPassword = await bcrypt.hash(request.body.password, 10);
+
+        const user = { username: request.body.username, name: request.body.name, password: hashedPassword };
 
         const newUser = new User(user);
         await newUser.save();
