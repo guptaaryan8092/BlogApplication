1b5ae724162f74923949ff072ee1608054b7b58f
guptaaryan8092 <guptaaryan8092@gmail.com>
2025-01-19 17:48:41 +0530

    added routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,27 @@
 import './App.css'
+import DataProvider from './context/DataProvider';
+
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
 //Components
 import Login from './Components/Account/Login'
+import Home from './Components/home/Home'
+
 
 function App() {
 
   return (
-    <div style={{marginTop: 70 }}>
-      <Login />
-    </div>
-  )
+    <DataProvider>
+      <BrowserRouter>
+        <div style={{ marginTop: 70 }}>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </DataProvider>
+  );
 }
 
 export default App
