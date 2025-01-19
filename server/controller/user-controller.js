cdef29b1e885457d5f052c7413f92ef36683e8cc
guptaaryan8092 <guptaaryan8092@gmail.com>
2025-01-19 17:50:48 +0530

    added encryption

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,6 +1,11 @@
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
 import User from '../model/user.js'
+import Token from '../model/token.js'
+
+dotenv.config();
 
 export const signupUser = async (request, response) => {
     try{
@@ -34,12 +39,12 @@ export const loginUser = async (request, response) => {
             const newToken = new Token({ token: refreshToken });
             await newToken.save();
         
-            response.status(200).json({ accessToken: accessToken, refreshToken: refreshToken,name: user.name, username: user.username });
+            return response.status(200).json({ accessToken: accessToken, refreshToken: refreshToken,name: user.name, username: user.username });
         
         } else {
             response.status(400).json({ msg: 'Password does not match' })
         }
     } catch (error) {
-        response.status(500).json({ msg: 'error while login the user' })
+        return response.status(500).json({ msg: 'error while login the user' })
     }
 }
