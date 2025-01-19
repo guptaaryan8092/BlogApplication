479877e1acc33782c3e14a6fa74734ed66dea061
guptaaryan8092 <guptaaryan8092@gmail.com>
2025-01-19 18:58:53 +0530

    PrivateRoute . Here aunthenticated user is allowed to access the pag.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,23 @@ import { useState } from 'react';
 
 import DataProvider from './context/DataProvider';
 
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Outlet, Navigate} from 'react-router-dom';
 
 //Components
 import Login from './Components/Account/Login'
 import Home from './Components/home/Home'
 import Header from './Components/header/Header'
+
+// Here aunthenticated user is allowed to access the page.
+const PrivateRoute = ({ isAuthenticated, ...props }) => {
+  return isAuthenticated ?
+    <>
+      <Header />
+      <Outlet />
+    </>
+    :
+    <Navigate replace to='/login' />
+}
 
 
 function App() {
@@ -17,11 +28,14 @@ function App() {
   return (
     <DataProvider>
       <BrowserRouter>
-        <Header />
+        
         <div style={{ marginTop: 70 }}>
           <Routes>
             <Route path='/login' element={<Login isUserAuthenticated={isUserAuthenticated} />} />
-            <Route path='/' element={<Home />} />
+
+            <Route path='/' element={<PrivateRoute isAuthenticated={isAuthenticated} />} >
+              <Route path='/' element={<Home />} />
+            </Route>
           </Routes>
         </div>
       </BrowserRouter>
