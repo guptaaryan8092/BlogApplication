b2a5722fbbc3e94a787e6b94985e14634b22112f
guptaaryan8092 <guptaaryan8092@gmail.com>
2025-02-09 01:26:01 +0530

    added post pic

--- client/src/Components/home/post/Post.jsx
+++ client/src/Components/home/post/Post.jsx
@@ -39,7 +39,7 @@ const Details = styled(Typography)`
 `;
 
 const Post = ({post}) => {
-    const url = post.picture ? post.picture : 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80';
+    const url = post.picture ? post.picture : '/Assets/Post/blog_post2.gif';
 
     return (
         <Container>
